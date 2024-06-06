8bbd1dbb2ae6ec688c20e2b0cd0de53c183e83b5
pmspergge <pmatiasspergge@gmail.com>
2024-06-05 23:01:10 -0300

    Se agrega la creacion de las tablas para la db, y se hacen los inserts. Tambien se deja funcional el login con credenciales.

--- app/src/main/java/com/example/clubdeportivo/MainActivity.kt
+++ app/src/main/java/com/example/clubdeportivo/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 //noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.*
-import androidx.compose.material3.Button
 import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.Alignment
@@ -17,7 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.Composable
-import androidx.core.content.ContextCompat.startActivity
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -26,11 +25,35 @@ import androidx.navigation.compose.rememberNavController
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("MainActivity", "onCreate ejecutado")
+        val dbHelper = SqlHelper(this)
+        initializeDatabase(dbHelper)
+
+        // Verificar que los datos se han insertado correctamente
+        val users = dbHelper.getAllUsers()
+        for (user in users) {
+            Log.d("MainActivity", "Usuario: ${user["nombre"]} ${user["apellido"]}, Usuario: ${user["usuario"]}")
+        }
         setContent {
             ClubDeportivoNavHost()
         }
+    }
 
+    private fun initializeDatabase(dbHelper: SqlHelper) {
+        // Insertar un administrador
+        dbHelper.insertPersona("Admin", "ApellidoAdmin", "DireccionAdmin", "12345678A", "1980-01-01", 1, 1, 1, "adminUser", "adminPassword")
 
+        // Insertar un cliente
+        dbHelper.insertPersona("Cliente", "ApellidoCliente", "DireccionCliente", "87654321C", "1995-05-15", 1, 1, 0, "clienteUser", "clientePassword")
+
+        // Insertar un cliente que no sea socio
+        dbHelper.insertPersona("ClienteNoSocio", "ApellidoClienteNoSocio", "DireccionClienteNoSocio", "87654321N", "1998-07-22", 1, 0, 0, "clienteNoSocioUser", "clienteNoSocioPassword")
+
+        // Insertar cuotas para los clientes
+        dbHelper.insertCuota(2, "Enero", "mensual", "2024-01-05", "2024-01", 1, 100.00, "2024-01-31")
+        dbHelper.insertCuota(2, "Febrero", "mensual", "2024-02-05", "2024-02", 2, 100.00, "2024-02-28")
+        dbHelper.insertCuota(3, "Enero", "mensual", "2024-01-10", "2024-01", 1, 150.00, "2024-01-31")
+        dbHelper.insertCuota(3, "Febrero", "mensual", "2024-02-10", "2024-02", 2, 150.00, "2024-02-28")
     }
 }
 
@@ -50,6 +73,7 @@ fun ClubDeportivoNavHost() {
 @Composable
 fun ClubDeportivoApp(navController: NavController) {
     val context = LocalContext.current
+    val dbHelper = SqlHelper(context)
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var showMessage by remember { mutableStateOf(false) }
@@ -77,10 +101,12 @@ fun ClubDeportivoApp(navController: NavController) {
         Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = {
             if (username.isNotBlank() && password.isNotBlank()) {
-                SharedPreferencesManager.saveUserData(username, "cliente", context)
-                navController.navigate("home") // Navega a la pantalla Home
-                val intent = Intent(context, Menu::class.java)
-                context.startActivity(intent) // Inicia la nueva actividad
+                if (dbHelper.getUser(username, password)) {
+                    navController.navigate("home") // Navega a la pantalla Home
+                } else {
+                    messageText = "Usuario o contraseña incorrectos."
+                    showMessage = true
+                }
             } else {
                 messageText = "Por favor, ingresa ambos campos."
                 showMessage = true
@@ -94,6 +120,7 @@ fun ClubDeportivoApp(navController: NavController) {
     }
 }
 
+@Preview("Home screen")
 @Composable
 fun HomeScreen() {
     Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
--- app/src/main/java/com/example/clubdeportivo/sqlHelper.kt
+++ app/src/main/java/com/example/clubdeportivo/sqlHelper.kt
@@ -1,29 +1,116 @@
 package com.example.clubdeportivo
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class sqlHelper(context: Context):SQLiteOpenHelper(context, "clubDeportivo.db", null, 1) {
+class SqlHelper(context: Context) : SQLiteOpenHelper(context, "clubDeportivo.db", null, 1) {
     override fun onCreate(db: SQLiteDatabase) {
-        val tableCreate = "CREATE TABLE Users" + "(id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(55), password VARCHAR(22))";
-        db.execSQL(tableCreate);
+        val createPersonaTable = """
+            CREATE TABLE persona (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre VARCHAR(50),
+                apellido VARCHAR(50),
+                direccion VARCHAR(100),
+                dni VARCHAR(20),
+                fecha_nacimiento DATE,
+                apto_fisico TINYINT(1),
+                socio TINYINT(1),
+                admin TINYINT(1),
+                usuario VARCHAR(50),
+                contrasena VARCHAR(255)
+            );
+        """
+        db.execSQL(createPersonaTable)
+
+        val createCuotaTable = """
+            CREATE TABLE cuota (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                persona_id INTEGER,
+                mes_dia VARCHAR(10),
+                tipo VARCHAR(10),
+                fecha_pago DATE,
+                periodo VARCHAR(50),
+                numero_cuota INTEGER,
+                monto DECIMAL(10, 2),
+                fecha_vencimiento DATE,
+                FOREIGN KEY (persona_id) REFERENCES persona(id)
+            );
+        """
+        db.execSQL(createCuotaTable)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        TODO("Not yet implemented")
+        db?.execSQL("DROP TABLE IF EXISTS persona")
+        db?.execSQL("DROP TABLE IF EXISTS cuota")
+        if (db != null) {
+            onCreate(db)
+        }
     }
 
-    fun InsertData(name: String, password: String){
-        val data = ContentValues();
-        data.put("name", name);
-        data.put("password", password);
+    fun insertPersona(name: String, surname: String, address: String, dni: String, birthDate: String, aptoFisico: Int, socio: Int, admin: Int, username: String, password: String) {
+        val values = ContentValues()
+        values.put("nombre", name)
+        values.put("apellido", surname)
+        values.put("direccion", address)
+        values.put("dni", dni)
+        values.put("fecha_nacimiento", birthDate)
+        values.put("apto_fisico", aptoFisico)
+        values.put("socio", socio)
+        values.put("admin", admin)
+        values.put("usuario", username)
+        values.put("contrasena", password)
 
-        val db = this.writableDatabase;
-        db.insert("Users", null, data);
-
-        db.close();
+        val db = this.writableDatabase
+        db.insert("persona", null, values)
+        db.close()
     }
 
+    fun insertCuota(personaId: Int, mesDia: String, tipo: String, fechaPago: String, periodo: String, numeroCuota: Int, monto: Double, fechaVencimiento: String) {
+        val values = ContentValues()
+        values.put("persona_id", personaId)
+        values.put("mes_dia", mesDia)
+        values.put("tipo", tipo)
+        values.put("fecha_pago", fechaPago)
+        values.put("periodo", periodo)
+        values.put("numero_cuota", numeroCuota)
+        values.put("monto", monto)
+        values.put("fecha_vencimiento", fechaVencimiento)
+
+        val db = this.writableDatabase
+        db.insert("cuota", null, values)
+        db.close()
+    }
+
+    @SuppressLint("Range")
+    fun getAllUsers(): List<Map<String, String>> {
+        val db = this.readableDatabase
+        val cursor = db.rawQuery("SELECT * FROM persona", null)
+        val users = mutableListOf<Map<String, String>>()
+        if (cursor.moveToFirst()) {
+            do {
+                val user = mapOf(
+                    "id" to cursor.getString(cursor.getColumnIndex("id")),
+                    "nombre" to cursor.getString(cursor.getColumnIndex("nombre")),
+                    "apellido" to cursor.getString(cursor.getColumnIndex("apellido")),
+                    "usuario" to cursor.getString(cursor.getColumnIndex("usuario"))
+                )
+                users.add(user)
+            } while (cursor.moveToNext())
+        }
+        cursor.close()
+        db.close()
+        return users
+    }
+
+    fun getUser(username: String, password: String): Boolean {
+        val db = this.readableDatabase
+        val cursor = db.rawQuery("SELECT * FROM persona WHERE usuario = ? AND contrasena = ?", arrayOf(username, password))
+        val exists = cursor.moveToFirst()
+        cursor.close()
+        db.close()
+        return exists
+    }
 }
