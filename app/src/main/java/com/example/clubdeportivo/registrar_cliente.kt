f4e38c06b6e290bddc8629c67a5fc87a361f0292
Daniela <danscarballo@gmail.com>
2024-06-23 19:35:51 -0300

    Agrega funcionalidad a la panalla de 'Registrar cliente'

--- app/src/main/java/com/example/clubdeportivo/registrar_cliente.kt
+++ app/src/main/java/com/example/clubdeportivo/registrar_cliente.kt
@@ -3,44 +3,42 @@ package com.example.clubdeportivo
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.ImageButton
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class registrar_cliente : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_registrar_cliente)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val myButton = findViewById<ImageButton>(R.id.button_registrar_clie_volver)
-        myButton.setOnClickListener {
+
+        val backButton = findViewById<ImageButton>(R.id.button_registrar_clie_volver)
+        backButton.setOnClickListener {
             val intent = Intent(this, HomeAdmin::class.java)
             startActivity(intent)
         }
-        val myButton2 = findViewById<Button>(R.id.button)
-        myButton2.setOnClickListener {
-            val dbHelper = SqlHelper(this);
-            dbHelper.insertPersona("John",
-                "Doe",
-                "123 Main St",
-                "123456789",
-                "19900101",
-                1,
-                1,
-                0,
-                "johndoe",
-                "password123");
+
+        val registerButton = findViewById<Button>(R.id.button)
+        registerButton.setOnClickListener {
+            val dbHelper = SqlHelper(this)
+            val nombre = findViewById<TextInputEditText>(R.id.textInputEditTextNombre).text.toString()
+            val apellido = findViewById<TextInputEditText>(R.id.textInputEditTextApellido).text.toString()
+            val direccion = findViewById<TextInputEditText>(R.id.textInputEditTextDireccion).text.toString()
+            val dni = findViewById<TextInputEditText>(R.id.textInputEditTextDNI).text.toString()
+            val fechaNacimiento = findViewById<TextInputEditText>(R.id.textInputEditTextFechaNacimiento).text.toString()
+            val aptoFisico = if (findViewById<CheckBox>(R.id.checkBox).isChecked) 1 else 0
+            val socio = if (findViewById<CheckBox>(R.id.checkBox2).isChecked) 1 else 0
+
+            dbHelper.insertPersona(nombre, apellido, direccion, dni, fechaNacimiento, aptoFisico, socio, 0, "defaultUser", "defaultPassword")
         }
-
-
-
-
     }
 }
